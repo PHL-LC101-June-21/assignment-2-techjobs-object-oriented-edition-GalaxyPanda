78a654d2ca39680780be15af16542fe5dddee10c
GalaxyPanda <kasikfranks@gmail.com>
2021-10-22 00:12:52 -0400

    Began to add tests

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,45 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.Job;
+import org.launchcode.techjobs.oo.Employer;
+import org.launchcode.techjobs.oo.Location;
+import org.launchcode.techjobs.oo.PositionType;
+import org.launchcode.techjobs.oo.CoreCompetency;
+
+import java.util.Objects;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+
+
+    @Test
+    public void testSettingJobId(){
+         Job job1 = new Job();
+         Job job2 = new Job();
+        assertNotEquals(job1, job2);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job job;
+        job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(job instanceof Job);
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertTrue(!Objects.equals(job1, job2));
+    }
 
 }
